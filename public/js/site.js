aedc6f15b7223701e8822e10f038604935d0e554
Edoardo Sabadelli <edoardo@sabadelli.it>
2012-09-22 23:01:15 +0200

    Cosmetic tweaks

--- public/js/site.js
+++ public/js/site.js
@@ -1,11 +1,15 @@
+/*
+    http://sabadelli.it/giulia
+*/
+
 $(function () {
 
 // hide the back side
 $('#back').hide();
 
+// flip the postcard
 $('#front, #back').on('click', function (e) {
     $(this).hide();
-
     $(this.id === 'front' ? '#back' : '#front').show();
 });
 
