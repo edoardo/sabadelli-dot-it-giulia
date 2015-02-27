1a2a46b6912346a8fd90650c041b739192fda983
Edoardo Sabadelli <edoardo@sabadelli.it>
2015-02-27 21:06:03 +0000

    Grunt tasks
    For now, generate concatenated/optimized/minimized CSS
    and copy CSS resources (fonts, images) in public/

---
+++ grunt/Gruntfile.js
@@ -0,0 +1,101 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    // Metadata.
+    pkg: grunt.file.readJSON('package.json'),
+    banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+      '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+      '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+      '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+      ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
+    // Task configuration.
+    concat: {
+      options: {
+        banner: '',
+        stripBanners: true
+      },
+      css: {
+        src: ['../css/fonts.css', '../css/less4.css'],
+        dest: '../tmp/css/style.css'
+      }
+    },
+    uglify: {
+      options: {
+        banner: ''
+      }
+    },
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        unused: true,
+        boss: true,
+        eqnull: true,
+        globals: {}
+      },
+      gruntfile: {
+        src: 'Gruntfile.js'
+      },
+      lib_test: {
+        src: ['lib/**/*.js', 'test/**/*.js']
+      }
+    },
+    cssmin: {
+      minify: {
+        files: {
+          '../public/css/style.css': '../tmp/css/style.css'
+        }
+      }
+    },
+    copy: {
+      css_media: {
+        expand: true,
+        mode: true,
+        timestamp: true,
+        cwd: '../css/media/',
+        src: '**',
+        dest: '../public/css/media/'
+      },
+      css_fonts: {
+        expand: true,
+        cwd: '../css/fonts/',
+        src: '**',
+        dest: '../public/css/fonts/'
+      }
+    },
+    nodeunit: {
+      files: ['test/**/*_test.js']
+    },
+    watch: {
+      gruntfile: {
+        files: '<%= jshint.gruntfile.src %>',
+        tasks: ['jshint:gruntfile']
+      },
+      lib_test: {
+        files: '<%= jshint.lib_test.src %>',
+        tasks: ['jshint:lib_test', 'nodeunit']
+      }
+    }
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-nodeunit');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+
+  // Default task.
+  grunt.registerTask('default', ['concat', 'cssmin']);
+
+};
